1091d40ea5fabdcd6ff877644173f3f916d632ce
Tristan Swadell <tswadell@google.com>
2019-03-21 10:51:14 -0700

    Doc edit

--- cel/options.go
+++ cel/options.go
@@ -83,7 +83,7 @@ func Declarations(decls ...*exprpb.Decl) EnvOption {
 // during type-checking.
 //
 // Note, it is still possible to have heterogeneous aggregates when provided as variables to the
-// expression.
+// expression or with unchecked expressions.
 func HomogeneousAggregateLiterals() EnvOption {
 	return func(e *env) (*env, error) {
 		e.enableDynamicAggregateLiterals = false
