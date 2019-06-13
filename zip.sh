80f9e76a1a45e2c260ff96fe7a7c2711c3893c99
victoriajyang <victoriajyang@google.com>
2019-06-13 16:11:08 -0700

    fix relative path

--- zip.sh
+++ zip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #zip -r ./tests.zip ~/go/src/github.com/google/cel-go/bazel-testlogs/*
-tar -czf ./tests.tar.gz ~/go/src/github.com/google/cel-go/bazel-testlogs/*
+tar -czf ./tests.tar.gz ./bazel-testlogs/*
