c3774455aad8a0e007c8cac684cc77a9119d0060
victoriajyang <victoriajyang@google.com>
2019-06-13 15:59:56 -0700

    using tar instead of zip

--- zip.sh
+++ zip.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-zip -r ./tests.zip ~/go/src/github.com/google/cel-go/bazel-testlogs/*
+#zip -r ./tests.zip ~/go/src/github.com/google/cel-go/bazel-testlogs/*
+tar -czf ./tests.tar.gz ~/go/src/github.com/google/cel-go/bazel-testlogs/*
