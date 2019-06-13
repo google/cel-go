f72d39842bd121693310d4d866c37d8361bbbae2
victoriajyang <victoriajyang@google.com>
2019-06-13 16:30:41 -0700

    Revert "copying files"
    This reverts commit c789ea87d8e0b5a810a10cce569ab2d9f5f90ead.

--- zip.sh
+++ zip.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 #zip -r ./tests.zip ~/go/src/github.com/google/cel-go/bazel-testlogs/*
-mkdir -p testlogs
-cp -r ~/go/src/github.com/google/cel-go/bazel-testlogs/* testlogs
-tar -czf ./tests.tar.gz testlogs
+tar -czf ./tests.tar.gz ./bazel-testlogs/*
