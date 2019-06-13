c789ea87d8e0b5a810a10cce569ab2d9f5f90ead
victoriajyang <victoriajyang@google.com>
2019-06-13 16:30:26 -0700

    copying files

--- zip.sh
+++ zip.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 #zip -r ./tests.zip ~/go/src/github.com/google/cel-go/bazel-testlogs/*
-tar -czf ./tests.tar.gz ./bazel-testlogs/*
+mkdir -p testlogs
+cp -r ~/go/src/github.com/google/cel-go/bazel-testlogs/* testlogs
+tar -czf ./tests.tar.gz testlogs
