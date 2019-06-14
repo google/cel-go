28634b78a698286aafdb080af556660bef1a0f81
victoriajyang <victoriajyang@google.com>
2019-06-13 17:13:30 -0700

    copied directory

--- zip.sh
+++ zip.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 #zip -r ./tests.zip ~/go/src/github.com/google/cel-go/bazel-testlogs/*
-tar -czf ./tests.tar.gz ./bazel-testlogs/*
+mkdir -p testlogs
+for folder in ~/go/src/github.com/google/cel-go/bazel-testlogs/*;do
+  cp -r "$folder" ./testlogs
+done
+tar -czf ./tests.tar.gz testlogs/
+
