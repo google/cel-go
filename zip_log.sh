ac72c786ed91808c46be3b5fd3283b27a8a7c612
victoriajyang <victoriajyang@google.com>
2019-06-14 15:06:08 -0700

    copious output

--- zip_log.sh
+++ zip_log.sh
@@ -2,7 +2,23 @@
 cd ~
 echo $PWD
 cd .cache
+echo $PWD
 cd bazel
-tar -czf ./tests.tar.gz ./_bazel_root/eab0d61a99b6696edb3d2aff87b585e8/execroot/cel_go/bazel-output/k8-fastbuild/testlogs
-
-
+echo $PWD
+ls
+cd _bazel_root
+echo $PWD
+ls
+cd eab0d61a99b6696edb3d2aff87b585e8
+echo $PWD
+ls
+cd cel_go
+echo $PWD
+ls
+cd bazel-output
+echo $PWD
+cd k8-fastbuild
+echo $PWD
+cd testlogs
+echo $PWD
+tar -czf ./tests.tar.gz .
