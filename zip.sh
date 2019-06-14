67c26a5fece7a660cfb0d1db56b9adf7b2c36b1c
victoriajyang <victoriajyang@google.com>
2019-06-14 11:06:07 -0700

    creating tar gz

--- zip.sh
+++ zip.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-#zip -r ./tests.zip ~/go/src/github.com/google/cel-go/bazel-testlogs/*
-mkdir -p testlogs
-echo ~
-echo $PWD
+cd ../../../../../../../../../../
+cd ./execroot/cel_go/
+#echo $TEST_TMPDIR
+mkdir -p $TEST_UNDECLARED_OUTPUTS_DIR/testlogs
 for folder in ./bazel-out/k8-fastbuild/testlogs/*;do
-  cp -r "$folder" ./testlogs
+  cp -r "$folder" $TEST_UNDECLARED_OUTPUTS_DIR/testlogs
 done
-tar -czf ./tests.tar.gz testlogs
+echo $PWD
+cd $TEST_UNDECLARED_OUTPUTS_DIR
+tar -czf ./tests.tar.gz ./testlogs
+#cp tests.tar.gz ~/go/src/github.com/google/cel-go/
 #rm -r testlogs
