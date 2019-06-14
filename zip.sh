49e6c6cd21b205902f47409bc13e45dcd6b23b77
victoriajyang <victoriajyang@google.com>
2019-06-14 15:49:25 -0700

    test

--- zip.sh
+++ zip.sh
@@ -7,8 +7,9 @@ for folder in ./bazel-out/k8-fastbuild/testlogs/*;do
   cp -r "$folder" $TEST_UNDECLARED_OUTPUTS_DIR/testlogs
 done
 echo $PWD
-cd ../../../../../../../../../../
+cd ../../../../../../../../
 echo $PWD
+ls
 tar -czf ./tests.tar.gz $TEST_UNDECLARED_OUTPUTS_DIR/testlogs
 #cp tests.tar.gz ~/.cache/bazel/_bazel_$USER
 #rm -r testlogs
