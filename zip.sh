2bca2cb688ef1e926282fb4d1af45590338dfe86
victoriajyang <victoriajyang@google.com>
2019-06-14 13:58:10 -0700

    trying to run shell on cloud not test

--- zip.sh
+++ zip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd ../../../../../../../../../../
-cd ./execroot/cel_go/
-#echo $PWD
+#cd ../../../../../../../../../../
+#cd ./execroot/cel_go/
+echo $PWD
 mkdir -p $TEST_UNDECLARED_OUTPUTS_DIR/testlogs
 for folder in ./bazel-out/k8-fastbuild/testlogs/*;do
   cp -r "$folder" $TEST_UNDECLARED_OUTPUTS_DIR/testlogs
