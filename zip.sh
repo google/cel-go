1b289fcff3e919751d545b834c9e17f8ed006eb1
victoriajyang <victoriajyang@google.com>
2019-06-14 16:50:47 -0700

    running both shells

--- zip.sh
+++ zip.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo $TEST_TMPDIR
 cd ../../../../../../../../../../
 cd ./execroot/cel_go/
 echo $PWD
@@ -6,15 +7,16 @@ mkdir -p $TEST_UNDECLARED_OUTPUTS_DIR/testlogs
 for folder in ./bazel-out/k8-fastbuild/testlogs/*;do
   cp -r "$folder" $TEST_UNDECLARED_OUTPUTS_DIR/testlogs
 done
+cd $TEST_UNDECLARED_OUTPUTS_DIR
+#echo $PWD
+#cd ../../../../../../
+#echo $PWD
+#cd .cache
+#echo $PWD
+#cd bazel
+#echo $PWD
+#cd _bazel_$USER
 echo $PWD
-cd ../../../../../../
-echo $PWD
-cd .cache
-echo $PWD
-cd bazel
-echo $PWD
-cd _bazel_root
-echo $PWD
-tar -czf ./tests.tar.gz $TEST_UNDECLARED_OUTPUTS_DIR/testlogs
+tar -czf ./tests.tar.gz ./testlogs
 #cp tests.tar.gz ~/.cache/bazel/_bazel_$USER
-#rm -r testlogs
+rm -r testlogs
--- zip_log.sh
+++ zip_log.sh
@@ -1,24 +1,4 @@
 #!/bin/bash
 cd ~
 echo $PWD
-cd .cache
-echo $PWD
-cd bazel
-echo $PWD
 ls
-cd _bazel_root
-echo $PWD
-ls
-cd eab0d61a99b6696edb3d2aff87b585e8
-echo $PWD
-ls
-cd cel_go
-echo $PWD
-ls
-cd bazel-output
-echo $PWD
-cd k8-fastbuild
-echo $PWD
-cd testlogs
-echo $PWD
-tar -czf ./tests.tar.gz .
