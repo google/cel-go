f68d1a01032f63892ac06cb6765398d1431332ab
victoriajyang <victoriajyang@google.com>
2019-06-13 17:28:23 -0700

    changed relative path

--- zip.sh
+++ zip.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #zip -r ./tests.zip ~/go/src/github.com/google/cel-go/bazel-testlogs/*
 mkdir -p testlogs
-for folder in ~/go/src/github.com/google/cel-go/bazel-testlogs/*;do
+for folder in ./bazel-testlogs/*;do
   cp -r "$folder" ./testlogs
 done
-tar -czf ./tests.tar.gz testlogs/
-
+tar -czf ./tests.tar.gz testlogs
+#rm -r testlogs
