5e74f4ecbb7cf56de52df332ddbf8323529e61e6
victoriajyang <victoriajyang@google.com>
2019-06-14 15:30:02 -0700

    change storage location

--- zip.sh
+++ zip.sh
@@ -7,8 +7,8 @@ for folder in ./bazel-out/k8-fastbuild/testlogs/*;do
   cp -r "$folder" $TEST_UNDECLARED_OUTPUTS_DIR/testlogs
 done
 echo $PWD
-cd $TEST_UNDECLARED_OUTPUTS_DIR
-tar -czf ~/tests.tar.gz ./testlogs
-echo ~
+cd ../../../../../../../../../../
+echo $PWD
+tar -czf ./tests.tar.gz $TEST_UNDECLARED_OUTPUTS_DIR/testlogs
 #cp tests.tar.gz ~/.cache/bazel/_bazel_$USER
 #rm -r testlogs
