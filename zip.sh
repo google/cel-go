52854c13fc0a5968eba2ff2ce50406b9f5f3cd3d
victoriajyang <victoriajyang@google.com>
2019-06-14 09:48:36 -0700

    path through bazel-out

--- zip.sh
+++ zip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #zip -r ./tests.zip ~/go/src/github.com/google/cel-go/bazel-testlogs/*
 mkdir -p testlogs
-for folder in ./bazel-testlogs/*;do
+for folder in ./bazel-out/k8-fastbuild/testlogs/*;do
   cp -r "$folder" ./testlogs
 done
 tar -czf ./tests.tar.gz testlogs
