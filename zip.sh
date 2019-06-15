d9fb73dc943b4d731d3822daa22595bd00b81f1d
victoriajyang <victoriajyang@google.com>
2019-06-14 17:39:20 -0700

    make sure things added

--- zip.sh
+++ zip.sh
@@ -5,6 +5,7 @@ mkdir -p $TEST_UNDECLARED_OUTPUTS_DIR/testlogs
 for folder in ./bazel-out/k8-fastbuild/testlogs/*;do
   cp -r "$folder" $TEST_UNDECLARED_OUTPUTS_DIR/testlogs
 done
-#cd $TEST_UNDECLARED_OUTPUTS_DIR
+cd $TEST_UNDECLARED_OUTPUTS_DIR
+ls -la
 #tar -czf ./tests.tar.gz ./testlogs
 #rm -r testlogs
--- zip_log.sh
+++ zip_log.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-echo workspace
 ls -la
-echo bazel-out
 cd bazel-out
 ls -la
-echo k8
 cd k8-fastbuild
 ls -la
-echo tests
 cd testlogs
 ls -la
-echo zip
 cd zip_test
 ls -la
 cd test.outputs
