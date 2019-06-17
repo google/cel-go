b32cabe89636d6436ea52782ae50121f924f8270
victoriajyang <victoriajyang@google.com>
2019-06-17 09:55:07 -0700

    display xml

--- zip_log.sh
+++ zip_log.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 ls -la
-cd bazel-out
-ls -la
-cd k8-fastbuild
-ls -la
-cd testlogs
-ls -la
-cd zip_test
-ls -la
-cd test.outputs
-ls -la
-
+#cd bazel-out
+#ls -la
+#cd k8-fastbuild
+#ls -la
+#cd testlogs
+#ls -la
+#cd zip_test
+#ls -la
+#cd test.outputs
+#ls -la
+cat ./bazel-out/k8-fastbuild/testlogs/zip_test/test.xml
