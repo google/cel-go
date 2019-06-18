f1ecdb405f5beae68f301d623bcfe0f8c0fe1e39
victoriajyang <victoriajyang@google.com>
2019-06-17 17:46:29 -0700

    update zip sh

--- zip.sh
+++ zip.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 touch tests.xml
 for folder in ./bazel-out/k8-fastbuild/testlogs/*;do
-  echo "$folder"
   for folder2 in "$folder"/*;do
-    echo "$folder2"
-    for file in "$folder2";do
+    for file in "$folder2"/*;do
       echo "$file"
       if [ "$file" = "test.xml" ]
       then
