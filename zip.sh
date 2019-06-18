73900452d2de58ebecc0e021a15149435712c1ab
victoriajyang <victoriajyang@google.com>
2019-06-18 14:20:42 -0700

    echo info

--- zip.sh
+++ zip.sh
@@ -3,8 +3,11 @@ touch tests.xml
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > tests.xml
 echo "<testsuites>" >> tests.xml
 for folder in ./bazel-out/k8-fastbuild/testlogs/*;do
+  echo "$folder"
   for folder2 in "$folder"/*;do
+    echo "$folder2"
     for file in "$folder2"/*;do
+      echo "$file"
       if [ "${file##*/}" = "test.xml" ]
       then
           sed -e'1d;2d;$d' "$file" >> tests.xml
