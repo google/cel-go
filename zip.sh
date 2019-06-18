840a75c59c102a7cc2f5473c08aebef73b31f1eb
victoriajyang <victoriajyang@google.com>
2019-06-17 18:16:45 -0700

    remove first line of all xml files

--- zip.sh
+++ zip.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 touch tests.xml
+echo "<?xml version="1.0" encoding="UTF-8"?>" >> tests.xml
 for folder in ./bazel-out/k8-fastbuild/testlogs/*;do
   for folder2 in "$folder"/*;do
     for file in "$folder2"/*;do
       echo "${file##*/}"
       if [ "${file##*/}" = "test.xml" ]
       then
-          cat "$file" >> tests.xml
+          sed -e'1d' "$file" >> tests.xml
       fi
     done
   done
