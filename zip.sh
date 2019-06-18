aac9f1f6c4dad4b917f829404ad150c15a053e16
victoriajyang <victoriajyang@google.com>
2019-06-18 09:54:29 -0700

    fix formatting

--- zip.sh
+++ zip.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 touch tests.xml
 echo "<?xml version="1.0" encoding="UTF-8"?>" >> tests.xml
+echo "<testsuites>" >> tests.xml
 for folder in ./bazel-out/k8-fastbuild/testlogs/*;do
   for folder2 in "$folder"/*;do
     for file in "$folder2"/*;do
       echo "${file##*/}"
       if [ "${file##*/}" = "test.xml" ]
       then
-          sed -e'1d' "$file" >> tests.xml
+          sed -e'2d' "$file" >> tests.xml
           cat tests.xml
       fi
     done
   done
 done
+echo "</testsuites>" >> tests.xml
 #ls -la
 #tar -czf ./tests.tar.gz ./testlogs
