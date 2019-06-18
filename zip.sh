65de6ac18df9b4efb3f96791373d4e51db63fac5
victoriajyang <victoriajyang@google.com>
2019-06-18 11:02:47 -0700

    add quotes

--- zip.sh
+++ zip.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 touch tests.xml
-echo "<?xml version="1.0" encoding="UTF-8"?>" > tests.xml
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > tests.xml
 echo "<testsuites>" >> tests.xml
-cat tests.xml
-echo "hello"
 for folder in ./bazel-out/k8-fastbuild/testlogs/*;do
   for folder2 in "$folder"/*;do
     for file in "$folder2"/*;do
