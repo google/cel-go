d790769b11efe06d3551cf53c597c9fc1a361870
victoriajyang <victoriajyang@google.com>
2019-06-19 13:33:34 -0700

    just running conformance tests

--- zip.sh
+++ zip.sh
@@ -3,20 +3,6 @@ touch tests.xml
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > tests.xml
 echo "<testsuites>" >> tests.xml
 for file in $(find ./bazel-out/k8-fastbuild/testlogs -name '*.xml');do
-  echo "$file"
   sed -e'1d;2d;$d' $file >> tests.xml
 done
-
-#for folder in ./bazel-out/k8-fastbuild/testlogs/*;do
-#  for folder2 in "$folder"/*;do
-#    for file in "$folder2"/*;do
-#      if [ "${file##*/}" = "test.xml" ]
-#      then
-#          sed -e'1d;2d;$d' "$file" >> tests.xml
-#      fi
-#    done
-#  done
-#done
 echo "</testsuites>" >> tests.xml
-cat tests.xml
-#tar -czf ./tests.tar.gz ./testlogs
