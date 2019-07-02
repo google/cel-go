c6b7d897d276d1cd2a9d74a241eaf4fb1db4e31f
victoriajyang <victoriajyang@google.com>
2019-07-02 15:24:50 -0700

    fixing failure xml

--- conformance/zip.sh
+++ conformance/zip.sh
@@ -2,7 +2,8 @@
 mkdir -p artifacts
 touch artifacts/junit_01.xml
 
-input="./bazel-out/k8-fastbuild/testlogs/conformance/ct/test.xml"
+#input="./bazel-out/k8-fastbuild/testlogs/conformance/ct/test.xml"
+input="test-verbose.xml"
 
 head -2 $input > artifacts/junit_01.xml
 echo "<testsuite>" >> artifacts/junit_01.xml
@@ -12,19 +13,23 @@ do
   testline=$(echo $line | cut -c1-3)
   extendline=$(echo $line | cut -c1-4)
   testcase='<testcase name="'
-  testend='">'
+  end='">'
   close_testcase="</testcase>"
+
+  failure='<failure message="'
+  close_failure="</failure>"
 
   if [ $testline = "---" ] && [ $extendline != '----' ]
   then
     status=$(echo $line | cut -c4-8)
     name=$(echo $line | tail -c +11 | head -c -9)
     echo "$testcase$name$testend" >> artifacts/junit_01.xml
-    echo $status >> artifacts/junit_01.xml
     if [ $status = "FAIL" ]
     then
       read line1
-      echo $line1 >> artifacts/junit_01.xml
+      echo "$failure$line1$end$close_failure" >> artifacts/junit_01.xml
+    else
+      echo $status >> artifacts/junit_01.xml
     fi
     echo $close_testcase >> artifacts/junit_01.xml
   fi
@@ -38,7 +43,7 @@ timestamp='"timestamp": '
 time_string="$timestamp$startdate$comma"
 
 result='"result": "'
-test_string=$(tail -n 4 artifacts/junit_01.xml | head -n 1 | cut -c1-4)
+test_string=$(tail -n 5 $input | head -n 1 | cut -c1-6)
 
 if [ $test_string = "PASS" ]
 then
