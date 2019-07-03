5fa575b54dd9a6986cc7aa98d3ca2bc41ee8a3d2
victoriajyang <victoriajyang@google.com>
2019-07-02 17:26:24 -0700

    remove test var

--- conformance/zip.sh
+++ conformance/zip.sh
@@ -2,8 +2,8 @@
 mkdir -p artifacts
 touch artifacts/junit_01.xml
 
-#input="./bazel-out/k8-fastbuild/testlogs/conformance/ct/test.xml"
-input="test.xml"
+input="./bazel-out/k8-fastbuild/testlogs/conformance/ct/test.xml"
+
 head -2 $input > artifacts/junit_01.xml
 echo "<testsuite>" >> artifacts/junit_01.xml
 
