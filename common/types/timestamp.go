3a242a13303a85348b624334305406a84a40a9eb
victoriajyang <victoriajyang@google.com>
2019-07-08 16:24:13 -0700

    log wd

--- common/types/timestamp.go
+++ common/types/timestamp.go
@@ -297,6 +297,8 @@ func timeZone(tz ref.Val, visitor timestampVisitor) timestampVisitor {
 						return &Err{err}
 					}
 				}
+				dir, err := os.Getwd()
+				log.Panic(dir)
 				data, err := ioutil.ReadFile("zoneinfo.zip")
 				if err != nil {
 					return &Err{err}
