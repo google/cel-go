8c73738f07878778736b26c543b6afe87c231c95
victoriajyang <victoriajyang@google.com>
2019-07-08 11:18:32 -0700

    log error

--- common/types/timestamp.go
+++ common/types/timestamp.go
@@ -290,7 +290,6 @@ func timeZone(tz ref.Val, visitor timestampVisitor) timestampVisitor {
 					return &Err{err}
 				}
 				last := strings.LastIndex(currdir, "/")
-				log.Printf(string(last))
 				log.Panic(string(currdir[last+1]))
 				if string(currdir[last+1]) != "cel_go" {
 					err := os.Chdir("../../../../../..")
