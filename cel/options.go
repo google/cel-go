aa97d7e8af4029843321aed4f56bbfb59578662e
Tristan Swadell <tswadell@google.com>
2020-04-29 22:20:06 -0700

    Fix the doc comments referring to the extant ClearBuiltins() option

--- cel/options.go
+++ cel/options.go
@@ -36,9 +36,6 @@ type EnvOption func(e *Env) (*Env, error)
 //
 // Clearing macros will ensure CEL expressions can only contain linear evaluation paths, as
 // comprehensions such as `all` and `exists` are enabled only via macros.
-//
-// Note: This option is a no-op when used with ClearBuiltIns, and must be used before Macros
-// if used together.
 func ClearMacros() EnvOption {
 	return func(e *Env) (*Env, error) {
 		e.macros = parser.NoMacros
@@ -68,7 +65,9 @@ func CustomTypeProvider(provider ref.TypeProvider) EnvOption {
 
 // Declarations option extends the declaration set configured in the environment.
 //
-// Note: This option must be specified after ClearBuiltIns if both are used together.
+// Note: Declarations will by default be appended to the pre-existing declaration set cnofigured
+// for the environment. The cel.NewEnv call builds on top of the standard CEL declarations. For
+// a purely custom set of declarations use cel.NewCustomEnv.
 func Declarations(decls ...*exprpb.Decl) EnvOption {
 	// TODO: provide an alternative means of specifying declarations that doesn't refer
 	// to the underlying proto implementations.
