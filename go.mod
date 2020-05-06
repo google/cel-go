3ecb90950332a5bc03aa007436d9391dc3e4c72a
Tristan Swadell <tswadell@google.com>
2020-05-06 15:11:19 -0700

    Introduce a sync.Pool for ANTLR Lexer / Parser instances (#348)
    * Introduce a sync.Pool for ANTLR Parser instances
    * Introduce sync.Pool instances for the Lexer as well.
    
    Note, the change brings in github.com/antlr/antlr4/pulls/2816 which updates the
    ANTLR Go runtime. Other applications using vendored code, or alternate sources
    of truth besides the main ANTLR GitHub repository will also need to be udpated
    for this change to work.

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/google/cel-go
 go 1.12
 
 require (
-	github.com/antlr/antlr4 v0.0.0-20190819145818-b43a4c3a8015
+	github.com/antlr/antlr4 v0.0.0-20200503195918-621b933c7a7f
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
 	github.com/golang/protobuf v1.3.4
 	github.com/google/cel-spec v0.4.0
--- parser/parser.go
+++ parser/parser.go
@@ -19,6 +19,7 @@ package parser
 import (
 	"fmt"
 	"strconv"
+	"sync"
 
 	"github.com/antlr/antlr4/runtime/Go/antlr"
 
@@ -69,12 +70,30 @@ type parser struct {
 	macros map[string]Macro
 }
 
-var _ gen.CELVisitor = (*parser)(nil)
+var (
+	_ gen.CELVisitor = (*parser)(nil)
+
+	lexerPool *sync.Pool = &sync.Pool{
+		New: func() interface{} {
+			return gen.NewCELLexer(nil)
+		},
+	}
+
+	parserPool *sync.Pool = &sync.Pool{
+		New: func() interface{} {
+			return gen.NewCELParser(nil)
+		},
+	}
+)
 
 func (p *parser) parse(expression string) *exprpb.Expr {
-	stream := antlr.NewInputStream(expression)
-	lexer := gen.NewCELLexer(stream)
-	prsr := gen.NewCELParser(antlr.NewCommonTokenStream(lexer, 0))
+	lexer := lexerPool.Get().(*gen.CELLexer)
+	lexer.SetInputStream(antlr.NewInputStream(expression))
+	defer lexerPool.Put(lexer)
+
+	prsr := parserPool.Get().(*gen.CELParser)
+	prsr.SetInputStream(antlr.NewCommonTokenStream(lexer, 0))
+	defer parserPool.Put(prsr)
 
 	lexer.RemoveErrorListeners()
 	prsr.RemoveErrorListeners()
