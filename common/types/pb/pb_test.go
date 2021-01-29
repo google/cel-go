79c3eb3b2340f3ba65b80e19da6335ef4cb7c0e9
Tristan Swadell <tswadell@google.com>
2021-01-28 18:59:35 -0800

    Ensure protodesc.FileDescriptor references are sourced from the global proto registry when possible

--- common/types/pb/pb_test.go
+++ common/types/pb/pb_test.go
@@ -20,7 +20,11 @@ import (
 	"reflect"
 	"testing"
 
+	"google.golang.org/protobuf/reflect/protodesc"
+	"google.golang.org/protobuf/reflect/protoreflect"
+
 	proto3pb "github.com/google/cel-go/test/proto3pb"
+	descpb "google.golang.org/protobuf/types/descriptorpb"
 )
 
 func TestDbCopy(t *testing.T) {
@@ -38,5 +42,48 @@ func TestDbCopy(t *testing.T) {
 	clone2 := clone.Copy()
 	if !reflect.DeepEqual(clone, clone2) {
 		t.Error("db.Copy() did not result in eqivalent objects.")
+	}
+}
+
+func TestProtoReflectRoundTrip(t *testing.T) {
+	msg := &proto3pb.TestAllTypes{SingleBool: true}
+	fdMap := CollectFileDescriptorSet(msg)
+	files := []*descpb.FileDescriptorProto{}
+	for _, fd := range fdMap {
+		files = append(files, protodesc.ToFileDescriptorProto(fd))
+	}
+	// Round-tripping from a protoreflect.FileDescriptor to a FileDescriptorSet and back
+	// will result in completely independent copies of the protoreflect.FileDescriptor
+	// whose values are incompatible with each other.
+	//
+	// This test showcases what happens when the protoregistry.GlobalFiles values are
+	// used when a given protoreflect.FileDescriptor is linked into the binary.
+	fds := &descpb.FileDescriptorSet{File: files}
+	pbReg, err := protodesc.NewFiles(fds)
+	if err != nil {
+		t.Fatalf("protodesc.NewFiles() failed: %v", err)
+	}
+	pbdb := NewDb()
+	pbReg.RangeFiles(func(fd protoreflect.FileDescriptor) bool {
+		_, err := pbdb.RegisterDescriptor(fd)
+		if err != nil {
+			t.Fatalf("pbdb.RegisterDecriptor(%v) failed: %v", fd, err)
+		}
+		return true
+	})
+	msgType, found := pbdb.DescribeType("google.expr.proto3.test.TestAllTypes")
+	if !found {
+		t.Fatal("pbdb.DescribeType(google.expr.proto3.test.TestAllTypes) failed")
+	}
+	boolField, found := msgType.FieldByName("single_bool")
+	if !found {
+		t.Fatal("msgType.FieldByName(single_bool) failed")
+	}
+	val, err := boolField.GetFrom(msg)
+	if err != nil {
+		t.Errorf("boolField.GetFrom(msg) failed: %v", err)
+	}
+	if val != true {
+		t.Errorf("got TestAllTypes.single_bool %v, wanted true", val)
 	}
 }
