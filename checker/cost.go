cf4de496715efce8a1c4718f72e8fc9446bf88ff
Joe Betz <jpbetz@google.com>
2022-02-17 19:11:08 -0500

    appease bazel, add missing copyright

--- checker/cost.go
+++ checker/cost.go
@@ -1,3 +1,19 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package checker defines functions to type-checked a parsed expression
+// against a set of identifier and function declarations.
 package checker
 
 import (
@@ -205,9 +221,7 @@ func (vs iterRangeScopes) peek(varName string) (int64, bool) {
 	return 0, false
 }
 
-// Cost estimates the cost of the parsed CEL expression.
-// The parsedExpr is expected to have passed type checking. If it has not been
-// type checked, the results are undefined.
+// Cost estimates the cost of the parsed and type checked CEL expression.
 func Cost(checker *exprpb.CheckedExpr, estimator CostEstimator) CostEstimate {
 	c := coster{
 		checker:    checker,
