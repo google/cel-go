7dc6551e525879abfb16aae2acb6f3208c38d2a0
Justin King <jcking@google.com>
2025-02-25 09:33:07 -0800

    Deal with error, unknown, custom opaques, and custom objects
    Signed-off-by: Justin King <jcking@google.com>

--- common/types/format.go
+++ common/types/format.go
@@ -1,9 +1,11 @@
 package types
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/google/cel-go/common/types/ref"
+	"github.com/google/cel-go/common/types/traits"
 )
 
 type formattable interface {
@@ -23,4 +25,18 @@ func formatTo(sb *strings.Builder, val ref.Val) {
 		fmtable.format(sb)
 		return
 	}
+	// All of the builtins implement formattable. Try to deal with traits.
+	if l, ok := val.(traits.Lister); ok {
+		formatList(l, sb)
+		return
+	}
+	if m, ok := val.(traits.Mapper); ok {
+		formatMap(m, sb)
+		return
+	}
+	// This could be an error, unknown, opaque or object.
+	// Unfortunately we have no consistent way of inspecting
+	// opaque and object. So we just fallback to fmt.Stringer
+	// and hope it is relavent.
+	fmt.Fprintf(sb, "%s", val)
 }
