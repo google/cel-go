8aab1428853236f6056fd8b4e8114dfde4dd3187
Thomas Desrosiers <tdesrosi@google.com>
2025-11-19 18:37:50 -0500

    upstreaming CIDR and IP support to cel-go

---
+++ ext/network.go
@@ -0,0 +1,531 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ext
+
+import (
+	"fmt"
+	"net"
+	"reflect"
+
+	"github.com/google/cel-go/cel"
+	"github.com/google/cel-go/common/types"
+	"github.com/google/cel-go/common/types/ref"
+	"github.com/google/cel-go/common/types/traits"
+)
+
+// Network returns a cel.EnvOption to configure extended functions for network
+// address parsing, inspection, and CIDR range manipulation.
+//
+// Note: This library defines global functions `ip`, `cidr`, `isIP`, and
+// `isCIDR`. If you are currently using variables named `ip` or `cidr`, these
+// functions will likely work as intended, however there is a chance for
+// collision.
+//
+// The library closely mirrors the behavior of the Kubernetes CEL network
+// libraries, treating IP addresses and CIDR ranges as opaque types with
+// specific member functions.
+//
+// # IP Addresses
+//
+// The `ip` function converts a string to an IP address (IPv4 or IPv6). If the
+// string is not a valid IP, an error is returned. The `isIP` function checks
+// if a string is a valid IP address without throwing an error.
+//
+//	ip(string) -> ip
+//	isIP(string) -> bool
+//
+// Examples:
+//
+//	ip('127.0.0.1')
+//	ip('::1')
+//	isIP('1.2.3.4') // true
+//	isIP('invalid') // false
+//
+// # CIDR Ranges
+//
+// The `cidr` function converts a string to a Classless Inter-Domain Routing
+// (CIDR) range. If the string is not valid, an error is returned. The `isCIDR`
+// function checks if a string is a valid CIDR notation.
+//
+//	cidr(string) -> cidr
+//	isCIDR(string) -> bool
+//
+// Examples:
+//
+//	cidr('192.168.0.0/24')
+//	cidr('::1/128')
+//	isCIDR('10.0.0.0/8') // true
+//
+// # IP Member Functions
+//
+// IP objects support various inspection methods.
+//
+//	<ip>.family() -> int
+//	<ip>.isCanonical() -> bool
+//	<ip>.isLoopback() -> bool
+//	<ip>.isGlobalUnicast() -> bool
+//	<ip>.isLinkLocalMulticast() -> bool
+//	<ip>.isLinkLocalUnicast() -> bool
+//	<ip>.isUnspecified() -> bool
+//
+// Note on Canonicalization: `isCanonical()` returns true if the input string
+// used to construct the IP matches the RFC 5952 canonical string representation
+// of that address.
+//
+// Examples:
+//
+//	ip('127.0.0.1').family() == 4
+//	ip('::1').family() == 6
+//	ip('127.0.0.1').isLoopback() == true
+//	ip('2001:db8::1').isCanonical() == true  // RFC 5952 format
+//	ip('2001:DB8::1').isCanonical() == false // Uppercase is not canonical
+//	ip('2001:db8:0:0:0:0:0:1').isCanonical() == false // Expanded is not canonical
+//
+// # CIDR Member Functions
+//
+// CIDR objects support containment checks and property extraction.
+//
+//	<cidr>.containsIP(ip|string) -> bool
+//	<cidr>.containsCIDR(cidr|string) -> bool
+//	<cidr>.ip() -> ip
+//	<cidr>.masked() -> cidr
+//	<cidr>.prefixLength() -> int
+//
+// Examples:
+//
+//	cidr('10.0.0.0/8').containsIP(ip('10.0.0.1')) == true
+//	cidr('10.0.0.0/8').containsIP('10.0.0.1') == true
+//	cidr('10.0.0.0/8').containsCIDR('10.1.0.0/16') == true
+//	cidr('192.168.1.5/24').ip() == ip('192.168.1.5')
+//	cidr('192.168.1.5/24').masked() == cidr('192.168.1.0/24')
+//	cidr('192.168.1.0/24').prefixLength() == 24
+func Network() cel.EnvOption {
+	return cel.Lib(&networkLib{})
+}
+
+const (
+	// Function names
+	isIPFunc             = "isIP"
+	isCIDRFunc           = "isCIDR"
+	ipFunc               = "ip"
+	cidrFunc             = "cidr"
+	familyFunc           = "family"
+	isCanonicalFunc      = "isCanonical"
+	isLoopbackFunc       = "isLoopback"
+	isGlobalUnicastFunc  = "isGlobalUnicast"
+	isUnspecifiedFunc    = "isUnspecified"
+	isLinkLocalMcastFunc = "isLinkLocalMulticast"
+	isLinkLocalUcastFunc = "isLinkLocalUnicast"
+	containsIPFunc       = "containsIP"
+	containsCIDRFunc     = "containsCIDR"
+	maskedFunc           = "masked"
+	prefixLengthFunc     = "prefixLength"
+	ipFromCIDRFunc       = "ip"
+)
+
+var (
+	// Definitions for the Opaque Types
+	networkIPType   = cel.ObjectType("network.IP", traits.ReceiverType)
+	networkCIDRType = cel.ObjectType("network.CIDR", traits.ReceiverType)
+)
+
+type networkLib struct{}
+
+func (*networkLib) LibraryName() string {
+	return "cel.lib.ext.network"
+}
+
+// CompileOptions implements the Library interface method.
+func (*networkLib) CompileOptions() []cel.EnvOption {
+	return []cel.EnvOption{
+		cel.Types(
+			networkIPType,
+			networkCIDRType,
+		),
+		// Global Checkers
+		cel.Function(isIPFunc,
+			cel.Overload("isIP_string", []*cel.Type{cel.StringType}, cel.BoolType,
+				cel.UnaryBinding(netIsIP)),
+		),
+		cel.Function(isCIDRFunc,
+			cel.Overload("isCIDR_string", []*cel.Type{cel.StringType}, cel.BoolType,
+				cel.UnaryBinding(netIsCIDR)),
+		),
+
+		// Constructors
+		cel.Function(ipFunc,
+			cel.Overload("ip_string", []*cel.Type{cel.StringType}, networkIPType,
+				cel.UnaryBinding(netIPString)),
+		),
+		cel.Function(cidrFunc,
+			cel.Overload("cidr_string", []*cel.Type{cel.StringType}, networkCIDRType,
+				cel.UnaryBinding(netCIDRString)),
+		),
+
+		// IP Member Functions
+		cel.Function(familyFunc,
+			cel.MemberOverload("ip_family", []*cel.Type{networkIPType}, cel.IntType,
+				cel.UnaryBinding(netIPFamily)),
+		),
+		cel.Function(isCanonicalFunc,
+			cel.MemberOverload("ip_isCanonical", []*cel.Type{networkIPType}, cel.BoolType,
+				cel.UnaryBinding(netIPIsCanonical)),
+		),
+		cel.Function(isLoopbackFunc,
+			cel.MemberOverload("ip_isLoopback", []*cel.Type{networkIPType}, cel.BoolType,
+				cel.UnaryBinding(netIPIsLoopback)),
+		),
+		cel.Function(isGlobalUnicastFunc,
+			cel.MemberOverload("ip_isGlobalUnicast", []*cel.Type{networkIPType}, cel.BoolType,
+				cel.UnaryBinding(netIPIsGlobalUnicast)),
+		),
+		cel.Function(isUnspecifiedFunc,
+			cel.MemberOverload("ip_isUnspecified", []*cel.Type{networkIPType}, cel.BoolType,
+				cel.UnaryBinding(netIPIsUnspecified)),
+		),
+		cel.Function(isLinkLocalMcastFunc,
+			cel.MemberOverload("ip_isLinkLocalMulticast", []*cel.Type{networkIPType}, cel.BoolType,
+				cel.UnaryBinding(netIPIsLinkLocalMulticast)),
+		),
+		cel.Function(isLinkLocalUcastFunc,
+			cel.MemberOverload("ip_isLinkLocalUnicast", []*cel.Type{networkIPType}, cel.BoolType,
+				cel.UnaryBinding(netIPIsLinkLocalUnicast)),
+		),
+
+		// CIDR Member Functions
+		cel.Function(containsIPFunc,
+			cel.MemberOverload("cidr_containsIP_ip", []*cel.Type{networkCIDRType, networkIPType}, cel.BoolType,
+				cel.BinaryBinding(netCIDRContainsIP)),
+			cel.MemberOverload("cidr_containsIP_string", []*cel.Type{networkCIDRType, cel.StringType}, cel.BoolType,
+				cel.BinaryBinding(netCIDRContainsIPString)),
+		),
+		cel.Function(containsCIDRFunc,
+			cel.MemberOverload("cidr_containsCIDR_cidr", []*cel.Type{networkCIDRType, networkCIDRType}, cel.BoolType,
+				cel.BinaryBinding(netCIDRContainsCIDR)),
+			cel.MemberOverload("cidr_containsCIDR_string", []*cel.Type{networkCIDRType, cel.StringType}, cel.BoolType,
+				cel.BinaryBinding(netCIDRContainsCIDRString)),
+		),
+		cel.Function(maskedFunc,
+			cel.MemberOverload("cidr_masked", []*cel.Type{networkCIDRType}, networkCIDRType,
+				cel.UnaryBinding(netCIDRMasked)),
+		),
+		cel.Function(prefixLengthFunc,
+			cel.MemberOverload("cidr_prefixLength", []*cel.Type{networkCIDRType}, cel.IntType,
+				cel.UnaryBinding(netCIDRPrefixLength)),
+		),
+		cel.Function(ipFromCIDRFunc,
+			cel.MemberOverload("cidr_ip", []*cel.Type{networkCIDRType}, networkIPType,
+				cel.UnaryBinding(netCIDRIP)),
+		),
+	}
+}
+
+func (*networkLib) ProgramOptions() []cel.ProgramOption {
+	return []cel.ProgramOption{}
+}
+
+// --- Implementation Logic ---
+
+func netIsIP(val ref.Val) ref.Val {
+	s, ok := val.(types.String)
+	if !ok {
+		return types.MaybeNoSuchOverloadErr(val)
+	}
+	return types.Bool(net.ParseIP(string(s)) != nil)
+}
+
+func netIsCIDR(val ref.Val) ref.Val {
+	s, ok := val.(types.String)
+	if !ok {
+		return types.MaybeNoSuchOverloadErr(val)
+	}
+	_, _, err := net.ParseCIDR(string(s))
+	return types.Bool(err == nil)
+}
+
+func netIPString(val ref.Val) ref.Val {
+	s, ok := val.(types.String)
+	if !ok {
+		return types.MaybeNoSuchOverloadErr(val)
+	}
+	str := string(s)
+	ip := net.ParseIP(str)
+	if ip == nil {
+		return types.NewErr("invalid ip address: %s", str)
+	}
+	// Store both the logic (IP) and the representation (str)
+	return ipValue{IP: ip, str: str}
+}
+
+func netCIDRString(val ref.Val) ref.Val {
+	s, ok := val.(types.String)
+	if !ok {
+		return types.MaybeNoSuchOverloadErr(val)
+	}
+	str := string(s)
+	// CHANGE: Capture 'ip' (the specific address) alongside 'ipNet'
+	ip, ipNet, err := net.ParseCIDR(str)
+	if err != nil {
+		return types.NewErr("invalid cidr range: %s", str)
+	}
+	return cidrValue{IPNet: ipNet, ExtraIP: ip, str: str}
+}
+
+func netIPFamily(val ref.Val) ref.Val {
+	ip, ok := val.(ipValue)
+	if !ok {
+		return types.MaybeNoSuchOverloadErr(val)
+	}
+	if ip.IP.To4() != nil {
+		return types.Int(4)
+	}
+	return types.Int(6)
+}
+
+func netIPIsCanonical(val ref.Val) ref.Val {
+	ip, ok := val.(ipValue)
+	if !ok {
+		return types.MaybeNoSuchOverloadErr(val)
+	}
+	// 1-to-1 Parity: Check if the input string matches the standard library output
+	return types.Bool(ip.str == ip.IP.String())
+}
+
+func netIPIsLoopback(val ref.Val) ref.Val {
+	ip, ok := val.(ipValue)
+	if !ok {
+		return types.MaybeNoSuchOverloadErr(val)
+	}
+	return types.Bool(ip.IP.IsLoopback())
+}
+
+func netIPIsGlobalUnicast(val ref.Val) ref.Val {
+	ip, ok := val.(ipValue)
+	if !ok {
+		return types.MaybeNoSuchOverloadErr(val)
+	}
+	return types.Bool(ip.IP.IsGlobalUnicast())
+}
+
+func netIPIsUnspecified(val ref.Val) ref.Val {
+	ip, ok := val.(ipValue)
+	if !ok {
+		return types.MaybeNoSuchOverloadErr(val)
+	}
+	return types.Bool(ip.IP.IsUnspecified())
+}
+
+func netIPIsLinkLocalMulticast(val ref.Val) ref.Val {
+	ip, ok := val.(ipValue)
+	if !ok {
+		return types.MaybeNoSuchOverloadErr(val)
+	}
+	return types.Bool(ip.IP.IsLinkLocalMulticast())
+}
+
+func netIPIsLinkLocalUnicast(val ref.Val) ref.Val {
+	ip, ok := val.(ipValue)
+	if !ok {
+		return types.MaybeNoSuchOverloadErr(val)
+	}
+	return types.Bool(ip.IP.IsLinkLocalUnicast())
+}
+
+func netCIDRContainsIP(lhs, rhs ref.Val) ref.Val {
+	cidr, ok := lhs.(cidrValue)
+	if !ok {
+		return types.MaybeNoSuchOverloadErr(lhs)
+	}
+	ip, ok := rhs.(ipValue)
+	if !ok {
+		return types.MaybeNoSuchOverloadErr(rhs)
+	}
+	return types.Bool(cidr.IPNet.Contains(ip.IP))
+}
+
+func netCIDRContainsIPString(lhs, rhs ref.Val) ref.Val {
+	cidr, ok := lhs.(cidrValue)
+	if !ok {
+		return types.MaybeNoSuchOverloadErr(lhs)
+	}
+	s, ok := rhs.(types.String)
+	if !ok {
+		return types.MaybeNoSuchOverloadErr(rhs)
+	}
+	ip := net.ParseIP(string(s))
+	if ip == nil {
+		return types.NewErr("invalid ip address: %s", s)
+	}
+	return types.Bool(cidr.IPNet.Contains(ip))
+}
+
+func netCIDRContainsCIDR(lhs, rhs ref.Val) ref.Val {
+	parent, ok := lhs.(cidrValue)
+	if !ok {
+		return types.MaybeNoSuchOverloadErr(lhs)
+	}
+	child, ok := rhs.(cidrValue)
+	if !ok {
+		return types.MaybeNoSuchOverloadErr(rhs)
+	}
+	ones1, _ := parent.IPNet.Mask.Size()
+	ones2, _ := child.IPNet.Mask.Size()
+	return types.Bool(parent.IPNet.Contains(child.IPNet.IP) && ones1 <= ones2)
+}
+
+func netCIDRContainsCIDRString(lhs, rhs ref.Val) ref.Val {
+	parent, ok := lhs.(cidrValue)
+	if !ok {
+		return types.MaybeNoSuchOverloadErr(lhs)
+	}
+	s, ok := rhs.(types.String)
+	if !ok {
+		return types.MaybeNoSuchOverloadErr(rhs)
+	}
+	_, childNet, err := net.ParseCIDR(string(s))
+	if err != nil {
+		return types.NewErr("invalid cidr range: %s", s)
+	}
+	ones1, _ := parent.IPNet.Mask.Size()
+	ones2, _ := childNet.Mask.Size()
+	return types.Bool(parent.IPNet.Contains(childNet.IP) && ones1 <= ones2)
+}
+
+func netCIDRMasked(val ref.Val) ref.Val {
+	cidr, ok := val.(cidrValue)
+	if !ok {
+		return types.MaybeNoSuchOverloadErr(val)
+	}
+	// Mask both the network struct AND our extra IP
+	maskedIP := cidr.IPNet.IP.Mask(cidr.IPNet.Mask)
+	newNet := &net.IPNet{IP: maskedIP, Mask: cidr.IPNet.Mask}
+
+	// In a masked CIDR, the specific IP is the same as the network IP
+	return cidrValue{IPNet: newNet, ExtraIP: maskedIP, str: newNet.String()}
+}
+
+func netCIDRPrefixLength(val ref.Val) ref.Val {
+	cidr, ok := val.(cidrValue)
+	if !ok {
+		return types.MaybeNoSuchOverloadErr(val)
+	}
+	ones, _ := cidr.IPNet.Mask.Size()
+	return types.Int(ones)
+}
+
+func netCIDRIP(val ref.Val) ref.Val {
+	cidr, ok := val.(cidrValue)
+	if !ok {
+		return types.MaybeNoSuchOverloadErr(val)
+	}
+	// Extract IP. Use String() to ensure we have a valid canonical string representation
+	// for the new IP object. Return the specific host IP, not the network address
+	return ipValue{IP: cidr.ExtraIP, str: cidr.ExtraIP.String()}
+}
+
+// --- Opaque Type Wrappers ---
+
+// ipValue implements ref.Val
+type ipValue struct {
+	net.IP
+	str string // Kept for isCanonical checks
+}
+
+func (i ipValue) ConvertToNative(typeDesc reflect.Type) (any, error) {
+	if typeDesc == reflect.TypeOf(net.IP{}) {
+		return i.IP, nil
+	}
+	if typeDesc.Kind() == reflect.String {
+		return i.IP.String(), nil
+	}
+	return nil, fmt.Errorf("unsupported type conversion to '%v'", typeDesc)
+}
+
+func (i ipValue) ConvertToType(typeValue ref.Type) ref.Val {
+	switch typeValue {
+	case types.StringType:
+		return types.String(i.IP.String())
+	case networkIPType:
+		return i
+	case types.TypeType:
+		return networkIPType
+	}
+	return types.NewErr("type conversion error from '%s' to '%s'", networkIPType, typeValue)
+}
+
+func (i ipValue) Equal(other ref.Val) ref.Val {
+	o, ok := other.(ipValue)
+	if !ok {
+		return types.ValOrErr(other, "no such overload")
+	}
+	// Correctness: Equality is based on the actual IP bytes,
+	// NOT the string representation.
+	// ip("127.0.0.1") == ip("127.000.000.001") -> True
+	return types.Bool(i.IP.Equal(o.IP))
+}
+
+func (i ipValue) Type() ref.Type {
+	return networkIPType
+}
+
+func (i ipValue) Value() any {
+	return i.IP
+}
+
+// cidrValue implements ref.Val
+type cidrValue struct {
+	*net.IPNet
+	ExtraIP net.IP // IP Address with host bits set
+	str     string
+}
+
+func (c cidrValue) ConvertToNative(typeDesc reflect.Type) (any, error) {
+	if typeDesc == reflect.TypeOf(&net.IPNet{}) {
+		return c.IPNet, nil
+	}
+	if typeDesc.Kind() == reflect.String {
+		return c.IPNet.String(), nil
+	}
+	return nil, fmt.Errorf("unsupported type conversion to '%v'", typeDesc)
+}
+
+func (c cidrValue) ConvertToType(typeValue ref.Type) ref.Val {
+	switch typeValue {
+	case types.StringType:
+		return types.String(c.IPNet.String())
+	case networkCIDRType:
+		return c
+	case types.TypeType:
+		return networkCIDRType
+	}
+	return types.NewErr("type conversion error from '%s' to '%s'", networkCIDRType, typeValue)
+}
+
+func (c cidrValue) Equal(other ref.Val) ref.Val {
+	o, ok := other.(cidrValue)
+	if !ok {
+		return types.ValOrErr(other, "no such overload")
+	}
+	// Correctness: Equality is based on the IP bytes AND the mask bytes.
+	return types.Bool(c.IPNet.IP.Equal(o.IPNet.IP) && c.IPNet.Mask.String() == o.IPNet.Mask.String())
+}
+
+func (c cidrValue) Type() ref.Type {
+	return networkCIDRType
+}
+
+func (c cidrValue) Value() any {
+	return c.IPNet
+}
---
+++ ext/network_test.go
@@ -0,0 +1,365 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ext
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/google/cel-go/cel"
+	"github.com/google/cel-go/common/types"
+)
+
+func TestNetwork_Success(t *testing.T) {
+	// These test cases are ported from kubernetes/staging/src/k8s.io/apiserver/pkg/cel/library
+	// to ensure 1-to-1 parity with the Kubernetes implementation.
+	tests := []struct {
+		name string
+		expr string
+		out  any
+	}{
+		// --- Global Checks (isIP, isCIDR) ---
+		{
+			name: "isIP valid IPv4",
+			expr: "isIP('1.2.3.4')",
+			out:  true,
+		},
+		{
+			name: "isIP valid IPv6",
+			expr: "isIP('2001:db8::1')",
+			out:  true,
+		},
+		{
+			name: "isIP invalid",
+			expr: "isIP('not.an.ip')",
+			out:  false,
+		},
+		{
+			name: "isIP with port (invalid)",
+			expr: "isIP('127.0.0.1:80')",
+			out:  false,
+		},
+		{
+			name: "isCIDR valid",
+			expr: "isCIDR('10.0.0.0/8')",
+			out:  true,
+		},
+		{
+			name: "isCIDR invalid mask",
+			expr: "isCIDR('10.0.0.0/999')",
+			out:  false,
+		},
+
+		// --- IP Constructors & Equality ---
+		{
+			name: "ip equality IPv4",
+			expr: "ip('127.0.0.1') == ip('127.0.0.1')",
+			out:  true,
+		},
+		{
+			name: "ip inequality",
+			expr: "ip('127.0.0.1') == ip('1.2.3.4')",
+			out:  false,
+		},
+		{
+			name: "ip equality IPv6 mixed case inputs",
+			// Logic check: The value is equal even if string rep was different
+			expr: "ip('2001:db8::1') == ip('2001:DB8::1')",
+			out:  true,
+		},
+
+		// --- Family ---
+		{
+			name: "family IPv4",
+			expr: "ip('127.0.0.1').family()",
+			out:  int64(4),
+		},
+		{
+			name: "family IPv6",
+			expr: "ip('::1').family()",
+			out:  int64(6),
+		},
+
+		// --- Canonicalization (Critical Feature) ---
+		{
+			name: "isCanonical IPv4 simple",
+			expr: "ip('127.0.0.1').isCanonical()",
+			out:  true,
+		},
+		{
+			// ::ffff:127.0.0.1 is valid, but canonical form is 127.0.0.1
+			name: "isCanonical IPv4-mapped IPv6 (valid but non-canonical)",
+			expr: "ip('::ffff:127.0.0.1').isCanonical()",
+			out:  false,
+		},
+		{
+			name: "isCanonical IPv6 standard",
+			expr: "ip('2001:db8::1').isCanonical()",
+			out:  true,
+		},
+		{
+			name: "isCanonical IPv6 uppercase (invalid)",
+			expr: "ip('2001:DB8::1').isCanonical()",
+			out:  false,
+		},
+		{
+			name: "isCanonical IPv6 expanded (invalid)",
+			expr: "ip('2001:db8:0:0:0:0:0:1').isCanonical()",
+			out:  false,
+		},
+
+		// --- IP Types (Loopback, Unspecified, etc) ---
+		{
+			name: "isLoopback IPv4",
+			expr: "ip('127.0.0.1').isLoopback()",
+			out:  true,
+		},
+		{
+			name: "isLoopback IPv6",
+			expr: "ip('::1').isLoopback()",
+			out:  true,
+		},
+		{
+			name: "isUnspecified IPv4",
+			expr: "ip('0.0.0.0').isUnspecified()",
+			out:  true,
+		},
+		{
+			name: "isUnspecified IPv6",
+			expr: "ip('::').isUnspecified()",
+			out:  true,
+		},
+		{
+			name: "isGlobalUnicast 8.8.8.8",
+			expr: "ip('8.8.8.8').isGlobalUnicast()",
+			out:  true,
+		},
+		{
+			name: "isLinkLocalMulticast",
+			expr: "ip('ff02::1').isLinkLocalMulticast()",
+			out:  true,
+		},
+
+		// --- CIDR Accessors ---
+		{
+			name: "cidr prefixLength",
+			expr: "cidr('192.168.0.0/24').prefixLength()",
+			out:  int64(24),
+		},
+		{
+			name: "cidr ip extraction",
+			expr: "cidr('192.168.0.0/24').ip() == ip('192.168.0.0')",
+			out:  true,
+		},
+		{
+			name: "cidr ip extraction (host bits set)",
+			// K8s behavior: cidr('1.2.3.4/24').ip() returns 1.2.3.4, not 1.2.3.0
+			expr: "cidr('192.168.1.5/24').ip() == ip('192.168.1.5')",
+			out:  true,
+		},
+		{
+			name: "cidr masked",
+			// masked() zeroes out the host bits
+			expr: "cidr('192.168.1.5/24').masked() == cidr('192.168.1.0/24')",
+			out:  true,
+		},
+		{
+			name: "cidr masked identity",
+			expr: "cidr('192.168.1.0/24').masked() == cidr('192.168.1.0/24')",
+			out:  true,
+		},
+
+		// --- Containment (IP in CIDR) ---
+		{
+			name: "containsIP simple",
+			expr: "cidr('10.0.0.0/8').containsIP(ip('10.1.2.3'))",
+			out:  true,
+		},
+		{
+			name: "containsIP string overload",
+			expr: "cidr('10.0.0.0/8').containsIP('10.1.2.3')",
+			out:  true,
+		},
+		{
+			name: "containsIP edge case (network address)",
+			expr: "cidr('10.0.0.0/8').containsIP(ip('10.0.0.0'))",
+			out:  true,
+		},
+		{
+			name: "containsIP edge case (broadcast)",
+			expr: "cidr('10.0.0.0/8').containsIP(ip('10.255.255.255'))",
+			out:  true,
+		},
+		{
+			name: "containsIP false",
+			expr: "cidr('10.0.0.0/8').containsIP(ip('11.0.0.0'))",
+			out:  false,
+		},
+
+		// --- Containment (CIDR in CIDR) ---
+		{
+			name: "containsCIDR exact match",
+			expr: "cidr('10.0.0.0/8').containsCIDR(cidr('10.0.0.0/8'))",
+			out:  true,
+		},
+		{
+			name: "containsCIDR subnet",
+			expr: "cidr('10.0.0.0/8').containsCIDR(cidr('10.1.0.0/16'))",
+			out:  true,
+		},
+		{
+			name: "containsCIDR string overload",
+			expr: "cidr('10.0.0.0/8').containsCIDR('10.1.0.0/16')",
+			out:  true,
+		},
+		{
+			name: "containsCIDR larger prefix (false)",
+			// /8 does not contain /4
+			expr: "cidr('10.0.0.0/8').containsCIDR(cidr('0.0.0.0/4'))",
+			out:  false,
+		},
+		{
+			name: "containsCIDR disjoint",
+			expr: "cidr('10.0.0.0/8').containsCIDR(cidr('11.0.0.0/8'))",
+			out:  false,
+		},
+		{
+			name: "containsCIDR different family",
+			expr: "cidr('10.0.0.0/8').containsCIDR(cidr('::1/128'))",
+			out:  false,
+		},
+	}
+
+	// Initialize the environment with the Network extension
+	env, err := cel.NewEnv(Network())
+	if err != nil {
+		t.Fatalf("cel.NewEnv(Network()) failed: %v", err)
+	}
+
+	for _, tst := range tests {
+		t.Run(tst.name, func(t *testing.T) {
+			ast, iss := env.Compile(tst.expr)
+			if iss.Err() != nil {
+				t.Fatalf("Compile(%q) failed: %v", tst.expr, iss.Err())
+			}
+
+			prg, err := env.Program(ast)
+			if err != nil {
+				t.Fatalf("Program(%q) failed: %v", tst.expr, err)
+			}
+
+			out, _, err := prg.Eval(cel.NoVars())
+			if err != nil {
+				t.Fatalf("Eval(%q) failed: %v", tst.expr, err)
+			}
+
+			// Convert the CEL result to a native Go value for comparison
+			got, err := out.ConvertToNative(reflect.TypeOf(tst.out))
+			if err != nil {
+				t.Fatalf("ConvertToNative failed for expr %q: %v", tst.expr, err)
+			}
+
+			if !reflect.DeepEqual(got, tst.out) {
+				t.Errorf("Expr %q result got %v, wanted %v", tst.expr, got, tst.out)
+			}
+		})
+	}
+}
+
+func TestNetwork_RuntimeErrors(t *testing.T) {
+	tests := []struct {
+		name        string
+		expr        string
+		errContains string
+	}{
+		{
+			name:        "ip constructor invalid",
+			expr:        "ip('999.999.999.999')",
+			errContains: "invalid ip address",
+		},
+		{
+			name:        "cidr constructor invalid",
+			expr:        "cidr('1.2.3.4')",
+			errContains: "invalid cidr range",
+		},
+		{
+			name:        "cidr constructor invalid mask",
+			expr:        "cidr('10.0.0.0/999')",
+			errContains: "invalid cidr range",
+		},
+		{
+			name:        "containsIP string overload invalid",
+			expr:        "cidr('10.0.0.0/8').containsIP('not-an-ip')",
+			errContains: "invalid ip address",
+		},
+		{
+			name:        "containsCIDR string overload invalid",
+			expr:        "cidr('10.0.0.0/8').containsCIDR('not-a-cidr')",
+			errContains: "invalid cidr range",
+		},
+	}
+
+	env, err := cel.NewEnv(Network())
+	if err != nil {
+		t.Fatalf("cel.NewEnv(Network()) failed: %v", err)
+	}
+
+	for _, tst := range tests {
+		t.Run(tst.name, func(t *testing.T) {
+			ast, iss := env.Compile(tst.expr)
+			if iss.Err() != nil {
+				// Note: We only check runtime errors here. Compile errors are unexpected
+				// because these functions accept strings, so type-check passes.
+				t.Fatalf("Compile(%q) failed unexpectedly: %v", tst.expr, iss.Err())
+			}
+
+			prg, err := env.Program(ast)
+			if err != nil {
+				t.Fatalf("Program(%q) failed: %v", tst.expr, err)
+			}
+
+			_, _, err = prg.Eval(cel.NoVars())
+			if err == nil {
+				t.Errorf("Expected runtime error for %q, got nil", tst.expr)
+				return
+			}
+
+			// CEL errors are sometimes wrapped, so we check substring
+			if !types.IsError(types.NewErr(err.Error())) {
+				// Just a sanity check that it is indeed a CEL-compatible error structure
+				// Not strictly necessary but good practice
+			}
+
+			// Standard substring check
+			gotErr := err.Error()
+			// We just check if the message contains the specific error text we return in network.go
+			found := false
+			// Note: The actual error might be wrapped in "evaluation error: ..."
+			if len(tst.errContains) > 0 {
+				// Simple string contains check
+				for i := 0; i < len(gotErr)-len(tst.errContains)+1; i++ {
+					if gotErr[i:i+len(tst.errContains)] == tst.errContains {
+						found = true
+						break
+					}
+				}
+			}
+
+			if !found {
+				t.Errorf("Expected error containing %q, got %q", tst.errContains, gotErr)
+			}
+		})
+	}
+}
